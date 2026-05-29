8f601477b7311d9b71d9845a8ef4b1367f033161
AntiXen <amitghosh.115127@gmail.com>
2026-05-29 15:31:39 +0000

    hardcoded vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { tanstackStartVite } from '@tanstack/start-vite-plugin'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig();
+export default defineConfig({
+  plugins: [
+    tanstackStartVite(),
+    react(),
+    tsconfigPaths(),
+  ],
+})rm
